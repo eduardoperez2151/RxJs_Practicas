1f932205428baeb524de21597fa9a979609dd953
Eduardo Perez <eduardo.perez.2151@gmail.com>
2020-10-01 14:19:26 -0300

    Exercise-1 Solution - visual improvement

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import {of, pipe} from "rxjs";
-import {map, takeUntil, tap} from "rxjs/operators";
+import {of} from "rxjs";
+import {map} from "rxjs/operators";
 
 
 (() => {
+    const htmlContentDiv = document.getElementById("content");
+
+    const createH1Element = (nombre: string) => {
+        let htmlH1Element = document.createElement('h1');
+        let h1TextContet = document.createTextNode(nombre);
+        htmlH1Element.appendChild(h1TextContet)
+        htmlContentDiv.appendChild(htmlH1Element);
+    }
+
     const capitalizar = (nombre: string) => nombre.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 
-    const nombres$ = of('batman', 'joker', 'doble cara', 'pingüino', 'hiedra venenosa');
-    nombres$.pipe(
-        map(capitalizar))
-        .subscribe(console.log)
+    of('batman', 'joker', 'doble cara', 'pingüino', 'hiedra venenosa')
+        .pipe(
+            map(capitalizar))
+        .subscribe(createH1Element)
 })();
